dd89d735e250367f0049dafa5952cd0b831488c3
doinel1a <doinel1atanasiu@gmail.com>
2024-03-28 18:06:53 +0100

    feat(nav): implement logo

--- packages/nextjs/components/nav/index.tsx
+++ packages/nextjs/components/nav/index.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import React from 'react';
+
+import Logo from './logo';
 
 export default function Navbar() {
   return (
     <header className='border-border relative flex h-20 w-full justify-between border-b px-5'>
-      <nav className='flex h-full items-center gap-x-5'>NAV</nav>
+      <nav className='flex h-full items-center gap-x-5'>
+        <Logo />
+      </nav>
     </header>
   );
 }
