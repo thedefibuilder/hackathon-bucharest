c9566ea6551bbf66c05de757e239fb659f2887ae
doinel1a <doinel1atanasiu@gmail.com>
2024-03-28 09:34:09 +0100

    feat: implement prisma util

---
+++ packages/nextjs/utils/db.ts
@@ -0,0 +1,14 @@
+import { env } from "../env";
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
