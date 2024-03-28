30229c5ccf920a389b102c73751c908b0ed0b90d
doinel1a <doinel1atanasiu@gmail.com>
2024-03-28 18:04:27 +0100

    refactor: replace header w/ nav

---
+++ packages/nextjs/components/nav/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <header className='border-border relative flex h-20 w-full justify-between border-b px-5'>
+      <nav className='flex h-full items-center gap-x-5'>NAV</nav>
+    </header>
+  );
+}
