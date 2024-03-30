f204443fa0cf869792db731838fe4ff7bfd4104a
doinel1a <doinel1atanasiu@gmail.com>
2024-03-30 06:01:30 +0100

    refactor: update metadata

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -12,41 +12,14 @@ import '~~/styles/globals.css';
 import ThemeProvider from '~~/components/theme-provider';
 import { Toaster } from '~~/components/ui/toast/toaster';
 
-const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : `http://localhost:${process.env.PORT || 3000}`;
-const imageUrl = `${baseUrl}/thumbnail.jpg`;
-
 export const metadata: Metadata = {
-  metadataBase: new URL(baseUrl),
-  title: {
-    default: 'Scaffold-ETH 2 App',
-    template: '%s | Scaffold-ETH 2'
-  },
-  description: 'Built with 🏗 Scaffold-ETH 2',
-  openGraph: {
-    title: {
-      default: 'Scaffold-ETH 2 App',
-      template: '%s | Scaffold-ETH 2'
-    },
-    description: 'Built with 🏗 Scaffold-ETH 2',
-    images: [
-      {
-        url: imageUrl
-      }
-    ]
-  },
-  twitter: {
-    card: 'summary_large_image',
-    images: [imageUrl],
-    title: {
-      default: 'Scaffold-ETH 2',
-      template: '%s | Scaffold-ETH 2'
-    },
-    description: 'Built with 🏗 Scaffold-ETH 2'
-  },
-  icons: {
-    icon: [{ url: '/favicon.png', sizes: '32x32', type: 'image/png' }]
+  title: 'DeFi Builder - ETH Bucharest',
+  description: 'DeFi Builder - ETH Bucharest',
+  icons: 'favicon.svg',
+  manifest: 'app.webmanifest',
+  robots: {
+    index: false,
+    follow: false
   }
 };
 
